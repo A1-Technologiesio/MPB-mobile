d1438510a5faf412eabe127a9952778989f7b189
Joseph Braide <braidej@gmail.com>
2022-09-16 20:28:32 +0100

    cashout main

---
+++ myposbook/lib/screens/create_records/cashout_records/cashout_main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:myposbook/constants.dart';
+import 'package:myposbook/screens/create_records/cashout_records/components/body.dart';
+
+class CreateCashoutMain extends StatelessWidget {
+  const CreateCashoutMain({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+            brandColor,
+            Color(0xff225560),
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+
+          stops: [
+            0.3,
+            1.0
+          ] 
+          ),
+        ),
+        child: CashoutCreateRecordBody()
+        ),
+    );
+  }
+}
