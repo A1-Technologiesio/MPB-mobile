96a13ff2c3505cb6573577ae16ff456d6ef6fd9a
Joseph Braide <braidej@gmail.com>
2022-10-01 22:31:37 +0100

    #61 transaction items function

---
+++ myposbook/lib/screens/dashboard_screens/other_screens/shared_components/transaction_items.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+transactionItem(
+  transactionTitle,
+  transactionDate,
+  transactionAmount,
+  transactionCharge, {
+  mediaHeight,
+  mediaWidth,
+}) =>
+    Padding(
+      padding: EdgeInsets.symmetric(
+        vertical: mediaHeight * 0.005,
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border(
+            bottom: BorderSide(
+              color: Colors.black12,
+            ),
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: mediaHeight * 0.015,
+            horizontal: mediaWidth * 0.02,
+          ),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        height: mediaHeight * 0.05,
+                        width: mediaWidth * 0.09,
+                        decoration: BoxDecoration(
+                          color: Color(0xff386FA4),
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        child: Icon(
+                          Icons.savings,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(width: mediaWidth * 0.04),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            transactionTitle,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w800,
+                              fontSize: 16,
+                            ),
+                          ),
+                          SizedBox(
+                            height: mediaHeight * 0.005,
+                          ),
+                          Text(transactionDate.toString())
+                        ],
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        transactionAmount,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w800,
+                          fontSize: 15,
+                        ),
+                      ),
+                      SizedBox(
+                        height: mediaHeight * 0.005,
+                      ),
+                      Text(transactionCharge),
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
