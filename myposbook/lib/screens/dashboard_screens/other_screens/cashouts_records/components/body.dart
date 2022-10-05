080e33fa64b2dfb384057416dcc68d7eb3a4d96b
Joseph Braide <braidej@gmail.com>
2022-10-05 12:27:43 +0100

    #61 major refactoring

--- myposbook/lib/screens/dashboard_screens/other_screens/cashouts_records/components/body.dart
+++ myposbook/lib/screens/dashboard_screens/other_screens/cashouts_records/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myposbook/screens/dashboard_screens/dashboard_main/components/amount_display/amount_display_section.dart';
+import 'package:myposbook/screens/dashboard_screens/other_screens/cashouts_records/components/record_period_pageview.dart';
 import 'package:myposbook/screens/dashboard_screens/other_screens/shared_components/transaction_items.dart';
 
 class CashoutRecordBody extends StatelessWidget {
@@ -10,44 +11,31 @@ class CashoutRecordBody extends StatelessWidget {
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
     return SafeArea(
-      child: Column(
-        children: [
-          // Heading and go back button
-          Row(
-            children: [
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.arrow_back,
-                ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.035),
+        child: ListView(
+          children: [
+            // Heading and go back button
+            SizedBox(
+              height: screenHeight * 0.12,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Cashout Records',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: screenHeight * 0.03,
+                    ),
+                  ),
+                ],
               ),
-              Text('Cashout Records'),
-            ],
-          ),
+            ),
 
-          // Amount row
-          AmountDisplaySection(
-            recordTitle: 'Amount',
-            recordAmount: 2000.toString(),
-            recordTotalCharge: 2000.toString(),
-            leftSideColor: Color(0xff27142A),
-            rightSideColor: Color(0xff225560),
-          ),
-
-          // Transactions section
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text('Transactions'),
-          ),
-          transactionItem(
-            'transactionTitle',
-            'transactionDate',
-            '2000',
-            '600',
-            mediaHeight: screenHeight,
-            mediaWidth: screenWidth,
-          )
-        ],
+            //
+            RecordButtonPageView(),
+          ],
+        ),
       ),
     );
   }
