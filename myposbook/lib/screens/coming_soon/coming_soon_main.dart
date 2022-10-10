2b7998f9672e1ef29d0b129fcab2500b0a4202bf
Joseph Braide <braidej@gmail.com>
2022-10-10 09:03:37 +0100

    #73 coming soon main

---
+++ myposbook/lib/screens/coming_soon/coming_soon_main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:myposbook/screens/coming_soon/components/body.dart';
+
+class ComingSoonMain extends StatelessWidget {
+  const ComingSoonMain({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ComingSoonBody(),
+    );
+  }
+}
