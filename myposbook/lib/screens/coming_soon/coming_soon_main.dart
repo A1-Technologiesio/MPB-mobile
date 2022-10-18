1dfc67bb1ddae4c8e493237d9d51d1556732faf4
Joseph Braide <braidej@gmail.com>
2022-10-18 23:26:55 +0100

    #73 constructor to make pages reusable

--- myposbook/lib/screens/coming_soon/coming_soon_main.dart
+++ myposbook/lib/screens/coming_soon/coming_soon_main.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:myposbook/constants.dart';
 import 'package:myposbook/screens/coming_soon/components/body.dart';
 
 class ComingSoonMain extends StatelessWidget {
-  const ComingSoonMain({Key? key}) : super(key: key);
+  final header;
+  final supportingText;
+  final imagePath;
+  const ComingSoonMain({
+    Key? key,
+    required this.header,
+    required this.supportingText,
+    required this.imagePath,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ComingSoonBody(),
+      appBar: AppBar(
+        backgroundColor: brandColor,
+        title: Center(
+            child: Text(
+          header,
+        )),
+      ),
+      body: ComingSoonBody(
+        header: header,
+        supportingText: supportingText,
+        imagePath: imagePath,
+      ),
     );
   }
 }
