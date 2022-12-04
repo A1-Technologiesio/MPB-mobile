d638d10b4bc1c5f743dabaa5ac2198313bb33539
Joseph Braide <braidej@gmail.com>
2022-12-04 12:38:51 +0100

    slide up modal to confirm or decline a removal

--- myposbook/lib/screens/dashboard_screens/other_screens/shared_components/transaction_details/body.dart
+++ myposbook/lib/screens/dashboard_screens/other_screens/shared_components/transaction_details/body.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:myposbook/constants.dart';
 import 'package:http/http.dart' as http;
+import 'package:myposbook/screens/dashboard_screens/dashboard_main/dashboard_main_screen.dart';
 
 class TransactionDetailsBody extends StatelessWidget {
   final String transactionID;
@@ -43,6 +44,54 @@ class TransactionDetailsBody extends StatelessWidget {
       );
     }
 
+    showModalSheet(transID) => showModalBottomSheet(
+          context: context,
+          backgroundColor: Colors.transparent,
+          builder: (BuildContext context) => Container(
+            height: screenHeight * 0.4,
+            decoration: BoxDecoration(color: Colors.white),
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: screenWidth * 0.18,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('Are you sure you want to delete this record ?'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // confirmation button
+                      SizedBox(
+                        width: screenWidth * 0.3,
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            removeTransactionItem(
+                              transID,
+                              context,
+                            );
+                          },
+                          child: Text('Yes'),
+                        ),
+                      ),
+
+                      // cancel button
+                      SizedBox(
+                        width: screenWidth * 0.3,
+                        child: OutlinedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text('No'),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
     return SafeArea(
       child: Padding(
         padding: EdgeInsets.symmetric(
@@ -51,24 +100,6 @@ class TransactionDetailsBody extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // Transaction details header
-            SizedBox(
-              width: double.infinity,
-              height: screenHeight * 0.1,
-              // decoration: BoxDecoration(color: Color(0xff000000)),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    'Transaction Details',
-                    style: TextStyle(
-                        // color: Colors.white,
-                        fontSize: screenHeight * 0.028),
-                  ),
-                ],
-              ),
-            ),
-
             // transaction details section
             FutureBuilder(
               builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
@@ -85,6 +116,35 @@ class TransactionDetailsBody extends StatelessWidget {
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
+                            // Transaction details header
+                            SizedBox(
+                              width: double.infinity,
+                              height: screenHeight * 0.1,
+                              // decoration: BoxDecoration(color: Color(0xff000000)),
+                              child: Row(
+                                // crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'Transaction Details',
+                                    style: TextStyle(
+                                        // color: Colors.white,
+                                        fontSize: screenHeight * 0.028),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {
+                                      showModalSheet(
+                                          decodeJson['transaction_id']);
+                                    },
+                                    icon: Icon(
+                                      Icons.delete_outline_rounded,
+                                      color: Colors.red[500],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
                             transactionDetailData(
                               'ID',
                               decodeJson['transaction_id'],
@@ -141,7 +201,7 @@ class TransactionDetailsBody extends StatelessWidget {
               width: double.infinity,
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red[500],
+                  backgroundColor: brandColor,
                 ),
                 onPressed: () => Navigator.pop(context),
                 child: Text('Close'),
@@ -153,6 +213,7 @@ class TransactionDetailsBody extends StatelessWidget {
     );
   }
 
+// Future function that call the api Endpoint to get transaction data
   Future getTransactionDetail(transactionID, context) async {
     // get the access token from secure Storage
     final storage = await secureStorage();
@@ -188,5 +249,67 @@ class TransactionDetailsBody extends StatelessWidget {
     }
 
     return response.body;
+  }
+}
+
+Future removeTransactionItem(transactionId, context) async {
+  // access token
+  final storage = await secureStorage();
+  final accessToken = storage['access_token'];
+
+  // build the delete url
+  final apiURL =
+      Uri.https(APIUrlRoot, 'api/transactions/delete/$transactionId/');
+
+  // headers
+  Map<String, String> requestHeaders = {
+    'Content-type': 'application/json',
+    // 'Accept': 'application/json',
+    'Authorization': 'Bearer $accessToken',
+  };
+
+  // send the delete request
+  http.Response response = await http.delete(
+    apiURL,
+    headers: requestHeaders,
+  );
+  // when the token is expired
+  if (response.statusCode == 401) {
+    // where the login credentials provided a 401 error
+    const errorSnackBar = SnackBar(
+      content: Text('Session Expired you have been logged out.'),
+      backgroundColor: Colors.red,
+      behavior: SnackBarBehavior.floating,
+      elevation: 10,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(errorSnackBar);
+    Navigator.pushNamed(context, '/login');
+  }
+  // print(response.body);
+  if (response.statusCode == 200) {
+    Navigator.pop(context);
+    const errorSnackBar = SnackBar(
+      content: Text('Successfully removed this record'),
+      backgroundColor: Colors.greenAccent,
+      behavior: SnackBarBehavior.floating,
+      elevation: 10,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(errorSnackBar);
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const DashboardMainScreen(),
+      ),
+    );
+    // return response.body;
+  } else if (response.statusCode == 400) {
+    Navigator.pop(context);
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const DashboardMainScreen(),
+      ),
+    );
   }
 }
